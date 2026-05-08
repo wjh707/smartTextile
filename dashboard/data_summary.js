81efcbf1aae098bdbc3bbbfcdb894c11ef5e791d
wjh707 <64563902+wjh707@users.noreply.github.com>
2026-05-08 20:37:01 +0800

    上传 dashboard/data_summary.js

---
+++ dashboard/data_summary.js
@@ -0,0 +1,10 @@
+window.DASHBOARD_SUMMARY = {
+  "total_events": 84,
+  "tech_count": 6,
+  "scene_count": 7,
+  "company_count": 26,
+  "segment_count": 5,
+  "standard_count": 12,
+  "policy_count": 20,
+  "updated_at": "2026-05-07"
+};
